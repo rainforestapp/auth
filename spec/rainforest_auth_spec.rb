e77a463179d681fb0812e586a5afd596d249c555
Russell Smith <russell.smith@ukd1.co.uk>
2013-01-08 12:52:20 +0000

    Initial spec for Auth

---
+++ spec/rainforest_auth_spec.rb
@@ -0,0 +1,65 @@
+describe RainforestAuth do
+
+  context "#new" do
+
+    it "stores the key" do
+      auth = RainforestAuth.new 'key'
+      auth.key.should == 'key'
+    end
+
+    it "returns itself" do
+      RainforestAuth.new('key').should be_an_instance_of RainforestAuth
+    end
+
+  end
+
+
+  context ".sign" do
+
+    before :all do
+      @auth = RainforestAuth.new('key')
+    end
+
+    it "returns the expected signature" do
+      @auth.sign 'test', {option: 1}
+    end
+
+  end
+
+
+  context ".verify" do
+
+    it "returns false for a bad signature"
+
+    it "returns true for a valid signature"
+
+  end
+
+
+  context ".run_if_valid" do
+
+    before :all do
+      @auth = RainforestAuth.new('key')
+      @object = "test"
+      @object.stub(:test) { 3 }
+    end
+
+    it "executes the block if there is a valid signature" do
+      @object.should_receive :test
+
+      @auth.run_if_valid('key', 'test', {option: 1}) {
+        @object.test
+      }
+    end
+
+    it "does not execute the block for invalid signatures" do
+      @object.should_not_receive :test
+
+      @auth.run_if_valid('key', 'testxx', {option: 1}) {
+        @object.test
+      }
+    end
+
+  end
+
+end
