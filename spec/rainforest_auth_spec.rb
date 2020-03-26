23ac63bcaeb1cee84435ab4d2c2c373a173a0aaf
Bartek Kruszczynski <bartek@rainforestapp.com>
2020-03-26 12:16:42 +0100

    RF-17380 new expect syntax

--- spec/rainforest_auth_spec.rb
+++ spec/rainforest_auth_spec.rb
@@ -4,11 +4,11 @@ describe RainforestAuth do
   context "#new" do
     it "stores the key" do
       auth = RainforestAuth.new('key')
-      auth.key.should == 'key'
+      expect(auth.key).to eq('key')
     end
 
     it "returns itself" do
-      RainforestAuth.new('key').should be_an_instance_of RainforestAuth
+      expect(RainforestAuth.new('key')).to be_an_instance_of RainforestAuth
     end
   end
 
@@ -22,20 +22,20 @@ describe RainforestAuth do
     end
 
     it "returns a url" do
-      @url[0..4].should == 'https'
+      expect(@url[0..4]).to eq('https')
     end
 
     it "has the correct run_id" do
-      @url_split[7].should == @run_id
+      expect(@url_split[7]).to eq(@run_id)
     end
 
     it "has the correct action" do
-      @url_split[8].should == 'before_run'
+      expect(@url_split[8]).to eq('before_run')
     end
 
     it "has the correct digest" do
       digest = @url_split[9]
-      @auth.verify(digest, 'before_run', {:run_id => @run_id}).should be_truthy
+      expect(@auth.verify(digest, 'before_run', {:run_id => @run_id})).to be_truthy
     end
   end
 
@@ -45,15 +45,15 @@ describe RainforestAuth do
     end
 
     it "returns the expected signature" do
-      @auth.sign('test', {:option => 1}).should == '65f2253344287b3c5634a1ce6163fb694b2280b1'
+      expect(@auth.sign('test', {:option => 1})).to eq('65f2253344287b3c5634a1ce6163fb694b2280b1')
     end
 
     it "changes the signature with different data" do
-      @auth.sign('test', {:option => 2}).should_not == '65f2253344287b3c5634a1ce6163fb694b2280b1'
+      expect(@auth.sign('test', {:option => 2})).not_to eq('65f2253344287b3c5634a1ce6163fb694b2280b1')
     end
 
     it "works with no options parameter" do
-      @auth.sign('test').should == 'd38f897889c808c021a8ed97d2caacdac48b8259'
+      expect(@auth.sign('test')).to eq('d38f897889c808c021a8ed97d2caacdac48b8259')
     end
 
     context 'key hash is nil' do
@@ -70,15 +70,15 @@ describe RainforestAuth do
     end
 
     it "returns the expected signature" do
-      @auth.sign_old('test', {:option => 1}).should == '5957ba2707a51852d32309d16184e8adce9c4d8e'
+      expect(@auth.sign_old('test', {:option => 1})).to eq('5957ba2707a51852d32309d16184e8adce9c4d8e')
     end
 
     it "changes the signature with different data" do
-      @auth.sign_old('test', {:option => 2}).should_not == '5957ba2707a51852d32309d16184e8adce9c4d8e'
+      expect(@auth.sign_old('test', {:option => 2})).not_to eq('5957ba2707a51852d32309d16184e8adce9c4d8e')
     end
 
     it "works with no options parameter" do
-      @auth.sign_old('test').should == '0a41bdf26fac08a89573a7f5efe0a5145f2730df'
+      expect(@auth.sign_old('test')).to eq('0a41bdf26fac08a89573a7f5efe0a5145f2730df')
     end
 
     context 'key is nil' do
@@ -96,26 +96,26 @@ describe RainforestAuth do
     end
 
     it "returns true for a valid signature" do
-      @auth.verify(@digest, 'test', {:option => 1}).should be_truthy
+      expect(@auth.verify(@digest, 'test', {:option => 1})).to be_truthy
     end
 
     it "returns true for a valid old signature" do
-      @auth.verify(@old_digest, 'test', {:option => 1}).should be_truthy
+      expect(@auth.verify(@old_digest, 'test', {:option => 1})).to be_truthy
     end
 
     it "returns false for a bad signature" do
-      @auth.verify(@digest, 'test', {:option => 2}).should be_falsey
+      expect(@auth.verify(@digest, 'test', {:option => 2})).to be_falsey
     end
 
     it "returns false for a bad old signature" do
-      @auth.verify(@old_digest, 'test', {:option => 2}).should be_falsey
+      expect(@auth.verify(@old_digest, 'test', {:option => 2})).to be_falsey
     end
 
     it "works with no options parameter" do
       #OLD
-      @auth.verify('0a41bdf26fac08a89573a7f5efe0a5145f2730df', 'test').should be_truthy
+      expect(@auth.verify('0a41bdf26fac08a89573a7f5efe0a5145f2730df', 'test')).to be_truthy
       #NEW
-      @auth.verify('d38f897889c808c021a8ed97d2caacdac48b8259', 'test').should be_truthy
+      expect(@auth.verify('d38f897889c808c021a8ed97d2caacdac48b8259', 'test')).to be_truthy
     end
   end
 
@@ -128,7 +128,7 @@ describe RainforestAuth do
     end
 
     it "executes the block if there is a valid signature" do
-      @object.should_receive :some_method
+      expect(@object).to receive(:some_method)
 
       @auth.run_if_valid(@digest, 'test', {:option => 1}) {
         @object.some_method
@@ -136,7 +136,7 @@ describe RainforestAuth do
     end
 
     it "does not execute the block for invalid signatures" do
-      @object.should_not_receive :some_method
+      expect(@object).not_to receive(:some_method)
 
       @auth.run_if_valid(@digest, 'test', {:option => 2}) {
         @object.some_method
